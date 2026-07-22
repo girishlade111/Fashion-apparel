073025ae2a4243bba7a946be94e37f98915be54f
Girish Lade <girishlade111@gmail.com>
2026-07-22 04:39:33 -0700

    feat: implement storefront layout with cart context, global navbar, and category data fetching

--- src/app/(storefront)/layout.tsx
+++ src/app/(storefront)/layout.tsx
@@ -21,6 +21,7 @@ export default async function StorefrontLayout({
   return (
     <CartProvider>
       <Navbar categories={categories} />
+      <CartDrawer />
       {children}
     </CartProvider>
   );
